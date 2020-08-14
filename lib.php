2733f8b30ceb8778f2a5255966c5449975858866
Damian Mantuano <damianm@camba.coop>
2020-08-14 12:56:31 -0300

    Added Argentinian Peso as currency

--- lib.php
+++ lib.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 class enrol_mercadopago_plugin extends enrol_plugin {
 
     public function get_currencies() {
-        $codes = array('COP');
+        $codes = array('COP', 'ARS');
         $currencies = array();
         foreach ($codes as $c) {
             $currencies[$c] = new lang_string($c, 'core_currencies');
